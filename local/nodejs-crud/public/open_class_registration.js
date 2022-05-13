25c9e787c4327aaf1c344d698e0d04b6e4c5b9d8
coppintr <coppintr@oregonstate.edu>
2022-05-13 11:11:55 -0700

    Added logout button and made class search include category, fee, description, and instructor.

--- local/nodejs-crud/public/open_class_registration.js
+++ local/nodejs-crud/public/open_class_registration.js
@@ -2,45 +2,89 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
 }
 
 function mySearchFunction() {
-  // Declare variables
-  var input, cards, collapsible, item, i, j, txtValue, card, instructor, description;
-  // User Input
-  input = document.getElementById("searchbar").value.toLowerCase();
-  // Grabs the parent element by id
-  cards = document.getElementById("cards");
-  // Individual item on list
-  collapsible = cards.getElementsByClassName("collapsible");
+    //feeSearch();
+    // Declare variables
+    var input, cards, item, i, j, txtValue, card;
+    var collapsible, category, instructor, fee;
+    var listArr = [];
+    // User Input
+    input = document.getElementById("searchbar").value.toLowerCase();
+    // Grabs the parent element by id
+    cards = document.getElementById("cards");
+    // Individual item on list
+    collapsible = cards.getElementsByClassName("collapsible");
 
-  instructor = cards.getElementsByClassName("item1-instructor");
+    category = cards.getElementsByClassName("item2-category");
 
-  description = cards.getElementsByClassName("item7-descrip");
+    instructor = cards.getElementsByClassName("item1-instructor");
 
-  card = cards.getElementsByClassName("card");
-  console.log(collapsible);
+    fee = cards.getElementsByClassName("item4-fee");
 
-  // Treats lists items like an array, where each item can be accessed through it's index
-  for (i = 0; i < collapsible.length; i++) {
-    item = collapsible[i];
-    console.log(item.innerHTML);
-    // Iterate over each list item to see if the value of the input, ignoring case, matches the inner text or inner html of the item.
-    txtValue = item.innerHTML;
-    if (txtValue.toLowerCase().indexOf(input) > -1) {
-      // Displays list items that are a match, and nothing if no match
-      card[i].style.display = "";
-    } else {
-      card[i].style.display = "none";
+    description = cards.getElementsByClassName("item7-descrip");
+
+    card = cards.getElementsByClassName("card");
+    console.log(collapsible);
+
+    var collectibles = [collapsible, category, instructor, fee, description];
+
+    // Treats lists items like an array, where each item can be accessed through it's index
+    for (j = 0; j < collectibles.length; j++) {
+        for (i = 0; i < collectibles[j].length; i++) {
+            item = collectibles[j][i];
+            //console.log(item.innerHTML);
+            // Iterate over each list item to see if the value of the input, ignoring case, matches the inner text or inner html of the item.
+            txtValue = item.innerHTML;
+            if (txtValue.toLowerCase().indexOf(input) > -1) {
+                // add list items to an array
+                listArr.push(card[i]);
+            } else {
+                card[i].style.display = "none";
+            }
+        }
     }
-  }
+
+    //loop through positive searches and display them.
+    for (i = 0; i < listArr.length; i++) {
+        listArr[i].style.display = "";
+
+    }
 }
+
+// function feeSearch() {
+//     var card, fee, input, cost, cards, i, item;
+
+//     input = document.getElementById("select-box-fee").value.toLowerCase();
+//     // Grabs the parent element by id
+//     card = document.getElementById("cards");
+
+//     fee = card.getElementsByClassName("item4-fee");
+
+//     cards = card.getElementsByClassName("card");
+
+//     for (i = 0; i < cards.length; i++) {
+//       item = cards[i];
+//             // Iterate over each list item to see if the value of the input, ignoring case, matches the inner text or inner html of the item.
+//             txtValue = item.innerHTML;
+//             if (txtValue.toLowerCase().indexOf(input) > -1) {
+//                 // add list items to an array
+//                 listArr.push(card[i]);
+//             } else {
+//                 card[i].style.display = "none";
+//             }
+//     }
+
+
+
+// }
